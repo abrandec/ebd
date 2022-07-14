ba574fcafd7d29939313833aac6d430f6261df55
e <abrandecarlo@protonmail.com>
2022-07-14 02:51:23 -0700

    updated name for function entry point

---
+++ src/ebd/disasm.h
@@ -0,0 +1,9 @@
+#ifndef DSM_H
+#define DSM_H
+
+// EVM bytecode disassembler
+// @param bytes: bytecode to disassemble
+// @param filename: filename to write disassembly to
+void disasm(char *bytes, char *filename);
+
+#endif
