36c60bc416d950b18295d9a41e2396be1bcd52db
e <abrandecarlo@protonmail.com>
2022-07-14 12:44:21 -0700

    updated disasm function

--- src/ebd/disasm.h
+++ src/ebd/disasm.h
@@ -1,9 +1,12 @@
 #ifndef DSM_H
 #define DSM_H
 
+#include <stdbool.h>
+
 // EVM bytecode disassembler
 // @param bytes: bytecode to disassemble
 // @param filename: filename to write disassembly to
-void disasm(char *bytes, char *filename);
+// @param output: true if output should be written to stdout, false if output should be written to file
+void disasm(char *bytes, char *filename, bool output);
 
 #endif
