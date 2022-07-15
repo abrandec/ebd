2ecc0884d1c687063a90c22552cfb83c1a92779d
e <abrandecarlo@protonmail.com>
2022-07-15 13:14:36 -0700

    misc

--- src/errors/errors.c
+++ src/errors/errors.c
@@ -6,69 +6,34 @@
 #include <stdlib.h>
 
 void custom_error(uint8_t err_code) {
-  printf(YELLOW "Error code: %X\n" RESET, err_code);
-
   switch (err_code) {
-
-  // ┌───────────────────┐
-  // │   STACK ERRORS    │
-  // └───────────────────┘
-  
-  case STACK_OVERFLOW:
-    printf("└ Stack overflow!\n");
-    break;
-  case STACK_UNDERFLOW:
-    printf("└ Stack underflow!\n");
-    break;
-  case STACK_INVALID_INDEX:
-    printf("└ Stack index invalid!\n");
-    break;
-  case STACK_NULL:
-    printf("└ Stack is NULL!\n");
-    break;
-
-  // ┌───────────────────┐
-  // │   VM ERRORS       │
-  // └───────────────────┘
-  case VM_INVALID_OPCODE:
-    printf("└ Invalid opcode!\n");
-    break;
-  case VM_INVALID_ARGUMENT:
-    printf("└ Invalid argument!\n");
-    break;
-  case VM_MEMORY_SIZE_EXCEEDED:
-    printf("└ Memory size exceeded!\n");
-    break;
-  case STOP_INSTRUCTION:
-    printf("└ Stop instruction!\n");
-    break;
-
   // ┌───────────────────┐
   // │   IO ERRORS       │
   // └───────────────────┘
   case IO_FILENAME_NULL:
-    printf("└ Filename is NULL!\n");
+    printf(YELLOW"Filename is NULL!\n"RESET);
     break;
   case IO_DATA_NULL:
-    printf("└ Data is NULL!\n");
+    printf(YELLOW"Data is NULL!\n"RESET);
     break;
   case IO_FILE_OPEN_FAIL:
-    printf("└ Error opening file!\n");
+    printf(YELLOW"Error opening file!\n"RESET);
     break;
   case IO_FILE_CREATION_ERR:
-    printf("└ Error creating file!\n");
+    printf(YELLOW"Error creating file!\n"RESET);
     break;
   case IO_WRITE_ACCESS_ERR:
-    printf("└ No access to parent directory!  Try running vevm with sudo.\n");
+    printf(YELLOW"No access to parent directory!  Try running vevm with sudo.\n"RESET);
     break;
   case IO_FOLDER_NAME_ALREADY_EXISTS:
-    printf("└ Folder name already exists!\n");
+    printf(YELLOW"Folder name already exists!\n"RESET);
     break;
   case IO_FOLDER_NAME_TOO_LONG:
-    printf("└ Folder name is too long!\n");
+    printf(YELLOW"Folder name is too long!\n"RESET);
     break;
   default:
-    printf("└ Unknown error!\n");
+    printf("Unknown error!\n"RESET);
     break;
   }
+  exit(1);
 }
